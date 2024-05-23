b1d0adc13e50fa664d537ca61a4139ad8ddcd710
Victor Ryan <vitorsilva3612@gmail.com>
2024-05-23 11:54:39 -0300

    feat: data_table_widget

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@ Lateral_bar
 
 ### 0.0.2
 dropdown_widget
+
+
+### 0.0.3
+data_table_widget
---
+++ lib/src/widgets/data_table_widget.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+
+
+
+/*
+Original version of Dayanne Xavier, github: https://github.com/DayXL
+Repository: https://github.com/DayXL/Atividades-de-POO-1
+Adapted file: https://github.com/DayXL/Atividades-de-POO-1/blob/main/receita9-10ab/lib/view/widgets.dart#L130
+*/
+
+void _empty(String){}
+
+
+class DataTableWidget extends HookWidget {
+  final _sortCallback;
+  final List objects;
+  final List columns;
+  final List? columnsNames;
+
+  const DataTableWidget({super.key, 
+    this.objects = const [],
+    this.columns = const [], 
+    this.columnsNames,
+    sortCallback
+    }):
+    _sortCallback = sortCallback ?? _empty ;
+
+  @override
+  Widget build(BuildContext context) {
+    final columnsNamesFinal = columnsNames ?? columns;
+
+    final sortAscending = useState(true);
+    final sortColumnIndex = useState(0);
+    return DataTable(
+      sortAscending: sortAscending.value,
+      sortColumnIndex: sortColumnIndex.value,
+      columns: columnsNamesFinal.map( 
+        (name) => DataColumn(
+          onSort: (columnIndex, ascending) {
+            sortColumnIndex.value = columnIndex;
+            sortAscending.value = !sortAscending.value;
+
+            _sortCallback(columnsNamesFinal[columnIndex]);
+          },
+          label: Expanded(
+            child: Text(name,
+              style: const TextStyle(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold
+              )
+            )
+          )
+        )
+      ).toList(),
+
+
+      rows: objects.map( 
+        (obj) => DataRow(
+            cells: columns.map(
+              (propName) => DataCell(Text(obj[propName]))
+            ).toList()
+          )
+        ).toList()
+    );
+  }
+}
