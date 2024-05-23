7c4697731c2d19e54bb3ebacb6359ffaae938735
Victor Ryan <vitorsilva3612@gmail.com>
2024-05-22 22:33:35 -0300

    feat: dropdownWidget

--- lib/flutter_vicr_widgets.dart
+++ lib/flutter_vicr_widgets.dart
@@ -1,3 +1,4 @@
 library flutter_vicr_widgets;
 
 export 'src/widgets/lateral_bar.dart';
+export 'src/widgets/dropdown_widget.dart';
---
+++ lib/src/widgets/dropdown_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+// Adapted from:
+// https://github.com/VictorRyan3612/Flutter_Music_Player_App/blob/7921e1a34241251fcde01de5c3c3e661ec503f69/music_player_app/lib/widgets/dropdown.dart
+
+class DropdownWidget extends HookWidget {
+  final List<String> menuItems;
+  final Function(String? newValue) onChanged;
+
+  const DropdownWidget({super.key, required this.onChanged, required this.menuItems});
+
+  @override
+  Widget build(BuildContext context) {
+
+    List<DropdownMenuItem<String>> dropdownMenuItems = menuItems.map(
+      (String value) => DropdownMenuItem<String>(
+        value: value,
+        child: Text(value),
+      ),
+    ).toList();
+
+    var textValue = useState<String?>(null);
+
+    return DropdownButton(
+      value: textValue.value,
+      hint: const Text('Choose'),
+      onChanged: (String? newValue) {
+        if (newValue != null) {
+          textValue.value = newValue;
+        }
+        onChanged(newValue);
+      },
+      items: dropdownMenuItems,
+    );
+  }
+}
